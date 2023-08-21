9b0feeffccc0d090b7ce6d7600c397d1f2298d6b
RafiWick <rafiwick@gmail.com>
2023-08-21 15:14:43 -0600

    user stories 5 and 6

--- CaravanMVC/Controllers/HomeController.cs
+++ CaravanMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CaravanMVC.Models;
+﻿using CaravanMVC.DataAccess;
+using CaravanMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,14 +8,23 @@ namespace CaravanMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CaravanMvcContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CaravanMvcContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult OverView()
+        {
+            ViewData["DistinctDestinations"] = _context.Passengers.Select(p => p.Destination).Distinct().ToList();
+            ViewData["AverageAge"] = Convert.ToInt32(_context.Passengers.Select(p => p.Age).Average());
             return View();
         }
 
